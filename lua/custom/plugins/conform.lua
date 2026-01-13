1873bce4bf4749e6223bbd0d0c18430bdb64b897
veryspry <ehlinger.matt@gmail.com>
2026-01-13 10:25:42 +0100

    added format disable/enable commmands.

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -21,6 +21,25 @@ M.format_opts = function()
   }
 end
 
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting globally
+    vim.g.disable_autoformat = true
+  else
+    vim.b.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -44,7 +63,9 @@ return {
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
 
-        if vim.api.nvim_buf_get_name(bufnr):match '%.ts$' then
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return nil
+        elseif vim.api.nvim_buf_get_name(bufnr):match '%.ts$' then
           return nil
         elseif M.disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
